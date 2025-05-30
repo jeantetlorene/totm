d31ce7d135d622db9e067b7fbdb65cbfcdd05b50
jeantetlorene <105348746+jeantetlorene@users.noreply.github.com>
2025-05-30 21:00:11 +0000

    Deploying to gh-pages from @ jeantetlorene/totm@ffba654d3db9baf7eb059b46cb9847957e983c65 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-programme-initiatives",
           title: "Programme Initiatives",
-          description: "A growing collection of your cool projects.",
+          description: "La collaboration de tous les experts de TOTM a conduit à l’élaboration du programme “Initiatives TOTM” initié en 2021, grâce au soutien du Ministère de la Transition Ecologique (MTE). Ce programme propose un large éventail d’initiatives visant à répondre aux priorités établies par les experts et portées par l’association TOTM dans le cadre de neuf thématiques répondant aux enjeux actuels. Leurs objectifs sont souvent complémentaires voire interdépendants, à l’image des enjeux de conservation des tortues marines.Ces initiatives sont destinées à s’inscrire dans les plans d’actions locaux et les stratégies internationales, sans s’y substituer. Par ses actions, le programme vise à activer les synergies possibles entre les différents territoires, en capitalisant sur leurs expériences respectives. Il a par ailleurs vocation à donner une dimension nationale, voire internationale, aux initiatives locales déjà mises en œuvre. Son contenu est amené à évoluer selon un cycle triennal, celui des colloques nationaux du GTMF, tout en s’adaptant aux défis émergents",
           section: "Navigation",
           handler: () => {
             window.location.href = "/totm/nosinitiatives/";
