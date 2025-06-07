660c5228c0acf8ab898d6f9f0dce4bbee1e88b2c
jeantetlorene <105348746+jeantetlorene@users.noreply.github.com>
2025-06-07 08:45:23 +0000

    Deploying to gh-pages from @ jeantetlorene/totm@62ebaf24fa57afe8d807dacc45051954efe43dcc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-nous-soutenir",
           title: "Nous soutenir",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/totm/noussoutenir/";
