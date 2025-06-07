408c5e8d70b57b85ea4cdf87139178f93234bfea
jeantetlorene <105348746+jeantetlorene@users.noreply.github.com>
2025-06-07 07:40:09 +0000

    Deploying to gh-pages from @ jeantetlorene/totm@439da40c8045d3deb3cf3edf2dbd7193705188de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -73,26 +73,26 @@ ninja.data = [{
             window.location.href = "/totm/nosactualites/2025/voeux-2025/";
           
         },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+      },{id: "post-recrutement-d-39-une-chargé-de-mision",
         
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: "Recrutement d&#39;une chargé de mision",
         
         description: "",
         section: "Posts",
         handler: () => {
           
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+            window.location.href = "/totm/nosactualites/2024/recrutement-cm/";
+          
+        },
+      },{id: "post-voeux-2024",
+        
+          title: "Voeux 2024",
+        
+        description: "L’année 2024 marque le début de la mise en œuvre du Programme Initiatives TOTM en faveur des tortues marines de tous nos territoires. Merci à tous pour votre soutien et meilleurs vœux pour 2024 !",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/totm/nosactualites/2024/voeux-2024/";
           
         },
       },{id: "books-the-godfather",
