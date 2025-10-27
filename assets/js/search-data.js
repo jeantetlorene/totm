601e40b911041f3f2ebd1c5af952e1f68fa453c0
jeantetlorene <105348746+jeantetlorene@users.noreply.github.com>
2025-10-27 10:42:08 +0000

    Deploying to gh-pages from @ jeantetlorene/totm@c2118c235762fcc3c4be9fda8c9d7fa9abe12270 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -327,7 +327,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/company/tortuesmarines/posts", "_blank");
+          window.open("https://www.linkedin.com/in/company/tortuesmarines", "_blank");
         },
       },{
       id: 'light-theme',
