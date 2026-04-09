2f963e52f2e4057a121d132123ae7b8a1e13e2d3
jeantetlorene <105348746+jeantetlorene@users.noreply.github.com>
2026-04-09 17:06:59 +0000

    Deploying to gh-pages from @ jeantetlorene/totm@798d688df19d48cadb8c3b0f72a72db67eaad60c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,6 +51,13 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/totm/en/projects/";
               },
+            },{id: "dropdown-our-projects-bis",
+              title: "Our Projects bis",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/totm/en/projects_1/";
+              },
             },{id: "dropdown-publications",
               title: "Publications",
               description: "",
@@ -71,6 +78,13 @@ ninja.data = [{
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/totm/nosprojets/";
+              },
+            },{id: "dropdown-nos-projets-bis",
+              title: "Nos Projets bis",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/totm/nosprojets_1/";
               },
             },{id: "dropdown-publications",
               title: "Publications",
@@ -371,6 +385,46 @@ ninja.data = [{
           description: "",
           section: "Nosinitiatives",handler: () => {
               window.location.href = "/totm/nosinitiatives/initiative-9/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/totm/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/totm/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/totm/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/totm/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/totm/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/totm/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/totm/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/totm/projects/9_project/";
             },},{id: "team-alexandra-le-moal",
           title: 'Alexandra Le Moal',
           description: "",
